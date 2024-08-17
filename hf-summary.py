1a6341a963cc8195ce1c55d74376e1e710cf1926
Sourabh Nyalkalkar <61457807+sourabhnk@users.noreply.github.com>
2024-08-17 14:16:08 +0530

    Update hf-summary.py

--- hf-summary.py
+++ hf-summary.py
@@ -45,8 +45,8 @@ def find_sections_with_heuristics(pdf_reader):
 
     return sections
 
-API_URL = "https://api-inference.huggingface.co/models/globuslabs/ScholarBERT"
-headers = {"Authorization": "Bearer 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"}
+API_URL = "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3.1-8B"
+headers = {"Authorization": "Bearer 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"}
 
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
